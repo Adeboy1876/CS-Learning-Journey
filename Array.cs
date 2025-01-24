98c5a32129a214a8610e329c634f355f833a4d30
Adeboy1876 <44889505+Adeboy1876@users.noreply.github.com>
2025-01-24 10:14:20 +0000

    Add files via upload
    Finding min and max in an array

---
+++ Array.cs
@@ -0,0 +1,25 @@
+﻿List<double> shoppingListprices = new List<double> { 34, 3, 5.50, 2.99, 8.49 };
+
+double totalPrice = 0;
+
+double mostExpensive = double.MinValue;
+double cheapest = double.MaxValue;
+
+
+foreach(double price in shoppingListprices)
+{
+    totalPrice += price;
+
+    if(price > mostExpensive)
+    {
+        mostExpensive = price;
+    }
+    if(price < cheapest)
+    {
+        cheapest = price;
+    }
+}
+
+Console.WriteLine($"Total Price: {totalPrice:C2}");
+Console.WriteLine($"Most expensive item: {mostExpensive:C2}");
+Console.WriteLine($"Cheapest item: {cheapest:C2}");
