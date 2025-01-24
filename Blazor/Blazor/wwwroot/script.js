750f72fcc04d2cfa1ffe95c97cb48c01a26a7401
Adeboy1876 <44889505+Adeboy1876@users.noreply.github.com>
2025-01-24 10:36:48 +0000

    Add files via upload

---
+++ Blazor/Blazor/wwwroot/script.js
@@ -0,0 +1,32 @@
+﻿// Toggle Dark mode
+function toggleDarkMode() {
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-mode");
+}
+//show alert in image click
+document.addEventListener("DOMContentLoaded", () => {
+    const image = document.querySelector("img");
+    image.addEventListener("click", () => {
+        alert("Stop clicking me, I'm trying to sleep!");
+    });
+});
+
+function addHobby() {
+    //get Unordered list by id
+    const hobbieslist = document.getElementById("hobbies-list");
+
+    const newHobby = prompt("Enter a new hobby :");
+
+    if (newHobby && newHobby.trim() !== "") {
+        //create a new list item
+        const li = document.createElement("li");
+        li.textContent = newHobby;
+
+        //optional styling
+
+        //append the new list
+        hobbieslist.appendChild(li);
+    } else {
+        alert("please enter a valid hobby")
+    }
+}
---
+++ Blazor/readme.txt
@@ -0,0 +1,8 @@
+My first blazor project. Experimenting on different components within Blazor, JavaScript and CSS by incorporating:
+1. A real time temperature converter that converts Celsius, Fahrenheit and Kelvin.
+2. A responsive "My Hobbies" page.
+3. Toggle Dark Mode functionality 
+3. A random image and table
+4. A counter that increments, decrements or reset on click of a button.
+5. A random textbox
+6. A div that displays a text on hover or when clicked
