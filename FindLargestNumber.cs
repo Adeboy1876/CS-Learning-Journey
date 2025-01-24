ac8d63767a9236dd97f3cf7c4ba06fd41d085ff4
Adeboy1876 <44889505+Adeboy1876@users.noreply.github.com>
2025-01-24 10:17:17 +0000

    Add files via upload
    A simple program using a method to find the largest number in an array of numbers by comparing every number with its nearest neighbour from the first to the last using a loop and assigning whichever is the largest at the end of the loop .

---
+++ FindLargestNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class FindLargestNumber
+{
+    static void Main()
+    {
+        //numbers to search
+        int[] numbers = { 10, 24, 52, 3, 78, 19, 44 };
+
+
+
+        //call the method to pring the largest number
+        int largest = findLargestNumber(numbers); 
+        Console.WriteLine($"The largest number is: {largest}");
+    }
+
+
+   public static int findLargestNumber(int[] numbers)
+    {
+        // initialise the largest variable with the first element
+
+        int largest = numbers[0];
+
+        //loop through the rest
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] > largest)
+            {
+                largest = numbers[i];
+            }
+            //Console.WriteLine($"new largest is : {largest}"); //to show each step);
+        }
+        return largest;
+    }
+}
