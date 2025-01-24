1719cdf5239d690d0a8665baafdcd0127eab1f9e
Adeboy1876 <44889505+Adeboy1876@users.noreply.github.com>
2025-01-24 10:22:46 +0000

    Add files via upload
    Simulation for making a cup of tea. - Unfinished

---
+++ MakeTeaSimulation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class MakeTea
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Let's make a cup of tea");
+
+
+        Boilwater();
+        /*AddTeaBag();
+        PourWater();
+        BrewTea();
+        AddMilkandSugar();
+        StirTea();
+        ServeTea();*/
+
+
+        Console.WriteLine("Enjoy your tea");
+    }
+    static void Boilwater()
+    {
+        int count = 1 ;
+        Console.WriteLine("Step 1: Boiling water...");
+
+        while (count <= 10)
+        {
+            System.Threading.Thread.Sleep(1000); // 1 second pause
+            Console.Write(count);
+            count++;
+        }
+        Console.WriteLine("\n Water has boiled");
+    }
+}
