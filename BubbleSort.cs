f13b35310167676bacb72148fae0a97c1852562d
Adeboy1876 <44889505+Adeboy1876@users.noreply.github.com>
2025-01-24 10:24:19 +0000

    Add files via upload
    Sort an array of numbers in ascending order by checking each number with its nearest neighbour and moving the smallest to the front

---
+++ BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+int[] numbers = {5, 3, 8, 4, 2, 43, 21, 6, 9, 18, 7};
+int count = 0; 
+
+
+//Bubble Sort
+for (int i = 0; i < numbers.Length; i++)
+{
+    for (int j = 0; j < numbers.Length - 1; j++)
+    {
+        //Console.WriteLine(numbers[i] - numbers[j]);
+        if (numbers[j] > numbers[j + 1])
+        {
+            int temp = numbers[j];
+            int temp1 = numbers[j + 1];
+
+            //swap number
+
+            numbers[j] = numbers[j + 1];
+            numbers[j + 1] = temp;
+        }
+    }
+}
+
+foreach (int n in numbers)
+{
+    Console.WriteLine($"Numbers[{count}]: {n}");
+    count ++;
+}
